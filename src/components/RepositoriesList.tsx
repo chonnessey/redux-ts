f0d5316a725d3538db741e38422139e79a2b9d1e
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-21 00:49:00 -0700

    create useActions hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
 
   const [term, setTerm] = useState('');
-  const dispatch = useDispatch();
+  const { searchRepositories } = useActions();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch(actionCreators.searchRepositories(term))
+    searchRepositories(term)
   }
   return (
     <div>
