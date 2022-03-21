3df0c00333fe4bd308f6493fdb91d4628c857efc
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-21 01:14:22 -0700

    add jsx to print out names of searched packagesto component

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
 
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.repositories);
 
+  console.log(data)
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     searchRepositories(term)
@@ -18,6 +21,10 @@ const RepositoriesList: React.FC = () => {
           onChange={e => setTerm(e.target.value)}/>
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && 
+        data.map((name) => <div key={name}>{name}</div>)}
     </div>
   )
 }
