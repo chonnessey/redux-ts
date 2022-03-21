3368fed7c42c7936f050c2c021a506a4ad8f3f20
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-20 23:57:10 -0700

    move logic to seperate folders for better organization

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,33 +1,10 @@
+import { ActionType } from '../action_types';
+import { Action } from '../actions'
 interface RepositoriesState {
   loading: boolean;
   error: string | null
   data: string[];
 };
-
-interface SearchRepositoriesAction {
-  type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-  payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-  type: ActionType.SEARCH_REPOSITORIES_ERROR;
-  payload: string;
-}
-
-type Action =
-| SearchRepositoriesAction 
-| SearchRepositoriesSuccessAction 
-| SearchRepositoriesErrorAction
-
-enum ActionType {
-  SEARCH_REPOSITORIES = 'search_repositories',
-  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
-}
 
 const reducer = (
   state: RepositoriesState, 
