55e52092a3ad8cb5f56857720fbabad770b86a6a
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-20 23:49:07 -0700

    move reducers folder inside state folder

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,48 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null
+  data: string[];
+};
+
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+type Action =
+| SearchRepositoriesAction 
+| SearchRepositoriesSuccessAction 
+| SearchRepositoriesErrorAction
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
+const reducer = (
+  state: RepositoriesState, 
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
